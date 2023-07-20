096c2ac4739c3dc77d7e7460f75d30749f6bf927
u1and0 <e01.ando60@gmail.com>
2023-07-20 17:10:00 +0900

    [feat] 選択した日付のページへ遷移する

--- main.py
+++ main.py
@@ -33,20 +33,22 @@ async def read_root(request: Request):
     ]
     print(thumbnail_filenames[-10:])
     return templates.TemplateResponse(
-        "index.html", {
+        "index.html",
+        {
             "request": request,
-            "thumbnail_filenames": thumbnail_filenames[-10:],
+            # 最新の10件を表示
+            "thumbnail_filenames": reversed(thumbnail_filenames[-10:]),
         })
 
 
 @app.get("/{date}", response_class=HTMLResponse)
 async def show_selected_date(request: Request, date: str):
     """指定した日付の画像を表示"""
-    dt = datetime.strptime(date, "%Y-%m-%d").strftime("%y%m%d")
+    # dt = datetime.strptime(date, "%Y-%m-%d").strftime("%y%m%d")
     thumbnail_filenames = [
         os.path.join(image_directory, filename)
         for filename in os.listdir(image_directory)
-        if filename.startswith(dt) and filename.endswith(".jpg")
+        if filename.startswith(date) and filename.endswith(".jpg")
     ]
     return templates.TemplateResponse(
         "index.html", {
